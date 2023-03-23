58a258d0afd57770582d323d976382a25947c2da
xyrlan; pedro <68228285+xyrlan@users.noreply.github.com>
2023-03-22 21:47:30 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"
+  ],
+  theme: {
+    fontFamily: {
+      'hanken': ['Hanken Grotesk', 'sans-serif']
+    },
+    
+    extend: {},
+    
+
+  plugins: [],
+}
+}
